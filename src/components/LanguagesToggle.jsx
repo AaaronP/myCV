35a3711bdc3801a14c21b7cc21259964d651eeca
Aaron <aaronperezwindows10@gmail.com>
2023-12-13 11:01:11 -0600

    fix internationalization

--- src/components/LanguagesToggle.jsx
+++ src/components/LanguagesToggle.jsx
@@ -7,12 +7,12 @@ export default function LanguagesToggle () {
     return (
         <div class="radio-inputs dark:bg-gray-500 dark:text-sky-100 text-slate-500 font-semibold">
             <label class="radio">
-                <input type="radio" name="radio" checked={currentLocation.includes('es')}/>
-                <a href={currentLocation.includes('en') ? getRelativeLocaleUrl('es') : getRelativeLocaleUrl('en')} class="name">Español</a>
+                <input type="radio" name="radio" checked={currentLocation === '/'}/>
+                <a href={currentLocation.includes('en') ? '/' : getRelativeLocaleUrl('en')} class="name">Español</a>
             </label>
             <label class="radio" >
                 <input type="radio" name="radio" checked={currentLocation.includes('en')}/>
-                <a  href={currentLocation.includes('en') ? getRelativeLocaleUrl('es') : getRelativeLocaleUrl('en')} class="name">Inglés</a>
+                <a  href={currentLocation.includes('en') ? '/' : getRelativeLocaleUrl('en')} class="name">Inglés</a>
             </label>
         </div>        
     )
