c227e8dcd6c0dcc0f4cc1e5ffbe22b7d8b9fddee
Aaron <aaronperezwindows10@gmail.com>
2023-12-20 12:07:48 -0600

    fixed text color bug

--- src/components/LanguagesToggle.jsx
+++ src/components/LanguagesToggle.jsx
@@ -5,12 +5,12 @@ export default function LanguagesToggle () {
     const currentLocation = window.location.pathname
 
     return (
-        <div class="radio-inputs dark:bg-gray-500 dark:text-sky-100 text-slate-500 font-semibold">
+        <div class="radio-inputs dark:bg-gray-500 text-slate-500 dark:text-slate-500 font-semibold">
             <label class="radio">
                 <input type="radio" name="radio" checked={currentLocation === '/'}/>
                 <a href={currentLocation.includes('en') ? '/' : getRelativeLocaleUrl('en')} class="name">Español</a>
             </label>
-            <label class="radio" >
+            <label class="radio">
                 <input type="radio" name="radio" checked={currentLocation.includes('en')}/>
                 <a  href={currentLocation.includes('en') ? '/' : getRelativeLocaleUrl('en')} class="name">Inglés</a>
             </label>
